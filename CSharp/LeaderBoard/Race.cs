385b714d846b90246d555c3855f0dd8acc6117e5
explorer14 <amanag1983@gmail.com>
2024-11-23 20:53:03 +0100

    Add observational comments

--- CSharp/LeaderBoard/Race.cs
+++ CSharp/LeaderBoard/Race.cs
@@ -2,9 +2,12 @@ using System.Collections.Generic;
 
 namespace TDDMicroExercises.LeaderBoard
 {
+    //OBS: Is this a completed race or in progress race or something else?
     public class Race
     {
+        // OBS: why are points hardcoded?
         private static readonly int[] Points = {25, 18, 15};
+        // OBS: why need a dictionary to maintain a Drive -> driverName mapping when you already have a List<Driver>?
         private readonly Dictionary<Driver, string> _driverNames = new Dictionary<Driver, string>();
         private readonly string _name;
 
@@ -18,10 +21,14 @@ namespace TDDMicroExercises.LeaderBoard
             {
                 var driverName = driver.Name;
 
+                //OBS: this is breaking LSP. If you have to typecast to get derivative specific information
+                // you've lost at polymorphism. Why can a self-driving car not use name and country instead?
                 var drivingCar = driver as SelfDrivingCar;
 
                 if (drivingCar != null)
                 {
+                    // OBS: this can be encapsulated within the SelfDrivingCar type otherwise it leaks driver knowledge
+                    // into Race.
                     driverName = "Self Driving Car - " + drivingCar.Country + " (" +
                                  drivingCar.AlgorithmVersion + ")";
                 }
@@ -29,6 +36,7 @@ namespace TDDMicroExercises.LeaderBoard
             }
         }
 
+        //OBS: Property name doesn't match what's being returned.
         public List<Driver> Results { get; }
 
         public Dictionary<Driver, string> DriverNames
@@ -41,11 +49,15 @@ namespace TDDMicroExercises.LeaderBoard
             return Results.FindIndex(d => Equals(d, driver));
         }
 
+        //OBS: Points depend on the position of the driver in the Results collection.
+        // This is the first time points are "awarded" completely hidden and implicitly
         public int GetPoints(Driver driver)
         {
             return Points[Position(driver)];
         }
 
+        //OBS: why does Race expose an API to return driver name when the Driver object
+        // already exposes the Name property?
         public string GetDriverName(Driver driver)
         {
             return DriverNames[driver];
---
+++ CSharp/Leaderboard.Tests/CharacterisationTests.cs
@@ -0,0 +1,66 @@
+﻿using Xunit;
+using Xunit.Abstractions;
+using Xunit.Sdk;
+
+namespace TDDMicroExercises.LeaderBoard.Tests;
+
+public class CharacterisationTests(ITestOutputHelper testOutputHelper)
+{
+    [Fact]
+    public void GivenOneRaceAndTwoDriversReturnRankings()
+    {
+        var leaderBoardWithOneRaceAndOneDriver = new Leaderboard(
+            new Race(name: "GP2024",
+                new Driver(name: "aman2",
+                    country: "DE"),
+                new Driver(name: "aman",
+                    country: "NL")));
+
+        // OBS: Rankings are drivers sorted by their name alphabetically ascending
+        // One on top is rank 1st, one on the bottom is rank last.
+        var rankings = leaderBoardWithOneRaceAndOneDriver.DriverRankings();
+        foreach (var ranking in rankings)
+        {
+            testOutputHelper.WriteLine(ranking);    
+        }
+    }
+    
+    [Fact]
+    public void GivenOneRaceAndTwoDriversReturnResults()
+    {
+        var leaderBoardWithOneRaceAndOneDriver = new Leaderboard(
+            new Race(name: "GP2024",
+                new Driver(name: "aman2",
+                    country: "DE"),
+                new Driver(name: "aman",
+                    country: "NL")));
+
+        var driverResults = leaderBoardWithOneRaceAndOneDriver.DriverResults();
+        foreach (var result in driverResults)
+        {
+            testOutputHelper.WriteLine($"{result.Key}: {result.Value}");    
+        }
+    }
+    
+    [Fact]
+    public void GivenMultipleRacesAndTwoDriversReturnResults()
+    {
+        var leaderBoardWithOneRaceAndOneDriver = new Leaderboard(
+            new Race(name: "GP2023",
+                new Driver(name: "john wick",
+                    country: "DE"),
+                new Driver(name: "aman",
+                    country: "NL")),
+            new Race(name: "GP2024",
+                new Driver(name: "john wick",
+                    country: "DE"),
+                new Driver(name: "aman",
+                    country: "NL")));
+
+        var driverResults = leaderBoardWithOneRaceAndOneDriver.DriverResults();
+        foreach (var result in driverResults)
+        {
+            testOutputHelper.WriteLine($"{result.Key}: {result.Value}");    
+        }
+    }
+}
