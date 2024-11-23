53e66cb1dae54fc8e13aa59874a88199bcbe8a29
explorer14 <amanag1983@gmail.com>
2024-11-24 00:23:10 +0100

    Use IReadOnlyCollection instead of List

--- CSharp/LeaderBoard/Race.cs
+++ CSharp/LeaderBoard/Race.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 
 namespace TDDMicroExercises.LeaderBoard
@@ -6,20 +7,21 @@ namespace TDDMicroExercises.LeaderBoard
     public class Race
     {
         private readonly string _name;
+        private readonly Driver[] _drivers;
 
         public Race(string name, params Driver[] drivers)
         {
             _name = name;
-            Drivers.AddRange(drivers);
+            _drivers = drivers;
         }
 
-        public List<Driver> Drivers { get; } = new();
+        public IReadOnlyCollection<Driver> Drivers => _drivers;
 
 
         //OBS: Points depend on the position of the driver in the Results collection.
         // This is the first time points are "awarded" completely hidden and implicitly
         public int GetPoints(Driver driver) => 
-            Drivers.IndexOf(driver) switch
+            Array.IndexOf(_drivers, driver) switch
             {
                 0 => 25,
                 1 => 18,
