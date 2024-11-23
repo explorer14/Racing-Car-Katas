5e0cae709eb64e546eec352c91f238eefa69a788
explorer14 <amanag1983@gmail.com>
2024-11-24 00:33:13 +0100

    Extend tests to cover all indexes

--- CSharp/Leaderboard.Tests/CharacterisationTests.cs
+++ CSharp/Leaderboard.Tests/CharacterisationTests.cs
@@ -18,7 +18,11 @@ public class CharacterisationTests(ITestOutputHelper testOutputHelper)
                     Country: "NL")),
             new Race(name: "GP2023",
                 new Driver(Name: "b",
-                    Country: "DE")));
+                    Country: "DE"),
+                new Driver(Name: "c",
+                    Country: "DE"),
+                new Driver(Name: "d",
+                    Country: "FR")));
 
         var driverResults = leaderBoardWithOneRaceAndOneDriver.DriverResults();
 
@@ -37,6 +41,16 @@ public class CharacterisationTests(ITestOutputHelper testOutputHelper)
             {
                 second.Key.Should().Be("a");
                 second.Value.Should().Be(18);
+            },
+            third => 
+            {
+                third.Key.Should().Be("c");
+                third.Value.Should().Be(18);
+            },
+            fourth => 
+            {
+                fourth.Key.Should().Be("d");
+                fourth.Value.Should().Be(15);
             });
     }
 }
